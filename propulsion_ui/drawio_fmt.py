469f7c73ed126d0f9fd88feebe169235161c665a
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2023-02-28 18:02:25 +0000

    hacky but works

--- propulsion_ui/drawio_fmt.py
+++ propulsion_ui/drawio_fmt.py
@@ -13,6 +13,7 @@ import json
 import xml.etree.ElementTree as ET
 import zlib, base64
 import urllib.parse
+import argparse
 
 def decode_diagram(raw: str) -> ET.Element:
     dec = zlib.decompressobj(-zlib.MAX_WBITS)
@@ -73,12 +74,111 @@ def add_links_to_groups(diagram: ET.Element) -> None:
             child.remove(subchild)
             mx_cell.append(subchild)
 
+# Returns a list of ids which have animations defined
+def get_animation_ids(diagram: ET.Element):
+    anim_ids = []
+
+    root = diagram.find("root")
+    for child in list(root):
+        rule = child.get("rule")
+        if rule is None:
+            continue
+        
+        events = child.get("events")
+        if events is None:
+            events = ""
+
+        events = events.split(",")
+        _id = child.get("id")
+        
+        anim_ids.append((_id, rule, events))
+    
+    return anim_ids
+
+def parse_rules(filename: str, template_filename: str):
+    with open(template_filename) as file:
+        template = json.load(file)
+
+    with open(filename) as file:
+        rules = json.load(file)
+
+    rules_dict = dict()
+
+    for rule in rules:
+        generated_rule = template.copy()
+
+        name = rule["name"]
+        if name in rules_dict:
+            raise Exception(f"Duplicate rule {name}")
+
+        generated_rule["alias"] = name
+        generated_rule["column"] = rule["data_source"]
+        generated_rule["invert"] = not rule["thresholds"]["ascending"]
+        
+        thresholds = []
+        colours = []
+        for pair in rule["thresholds"]["values"]:
+            value = pair["value"] 
+            if value != "base":
+                thresholds.append(value)
+            colours.append(pair["colour"])
+        
+        generated_rule["colors"] = colours
+        generated_rule["thresholds"] = thresholds
+        
+        events = []
+        base_event = {"hidden": False, "pattern": ""}
+        for event in rule["events"]:
+            events.append(base_event | event) # Only works in python 3.9+
+        generated_rule["eventData"] = events
+        
+        rules_dict[name] = generated_rule
+    return rules_dict
+
+def expand_rules(rules, diagram):
+    for (id, rule_name, events) in get_animation_ids(diagram):
+        if rule_name not in rules:
+            raise Exception(f"Rule {rule_name} not found in rules.json")
+        
+        rule = rules[rule_name]
+
+        newEventData = [] # This is disgusting
+        for event in events:
+            if event == "colour":
+                shapeData = {
+                    "colorOn": "a",
+                    "hidden": False,
+                    "pattern": id,
+                    "style": "fillColor"
+                }
+                rule["shapeData"].append(shapeData)
+            
+            eventCfg = [ev.copy() for ev in rule["eventData"] if ev["style"] == event][0]
+            eventCfg["pattern"] = id
+            newEventData.append(eventCfg)
+        
+        rule["eventData"] = newEventData
+        rules[rule_name] = rule
+    return rules
 
 if __name__ == "__main__":
-    raw = get_diagram_from_html("embed-no-links.html")
+    raw = get_diagram_from_html("propped.html")
     diagram = decode_diagram(raw)
 
     add_links_to_groups(diagram)
+    
+    rules = parse_rules("rules.json", "rule_template.json")
 
-    insert_into_template("embed-template.html", "embed-test1.html", diagram)
-    ET.dump(diagram)
+    rules_data = expand_rules(rules, diagram)
+
+    with open("dashboard_template.json") as file:
+        dashboard_template = json.load(file)
+
+    for panel in dashboard_template["panels"]:
+        if "flowchartsData" in panel:
+            panel["flowchartsData"]["flowcharts"][0]["xml"] = raw
+            panel["rulesData"]["rulesData"] = rules_data
+            break
+
+    with open("out.json", "w") as file:
+        json.dump(dashboard_template, file)
