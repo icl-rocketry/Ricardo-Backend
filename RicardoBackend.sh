e2b63f20255f378c3b01b0d70fa807595c9faf44
kiran <kd619@ic.ac.uk>
2023-03-30 20:09:06 +0100

    fixed docker compse and dockerfile for new backend version

--- RicardoBackend.sh
+++ RicardoBackend.sh
@@ -7,10 +7,9 @@ python3 main.py \
         --baud $BAUD \
         --flask-host $FLASK_HOST \
         --flask-port $FLASK_PORT \
-        --redis-host $REDIS_HOST \
-        --redis-port $REDIS_PORT \
         --monitor-ip $MONITOR_IP \
         --monitor-port $MONITOR_PORT\
-&
-sleep 5
-python3 ricardobackend/websocketforwarder/websocketforwarder.py --sio_host ricardo-backend --sio_port 1337 --ws_host 0.0.0.0 --ws_port 1338
+        --ws_port $WS_PORT\
+        --ws_host $WS_HOST\
+        -v
+
