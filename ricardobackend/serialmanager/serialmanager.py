57decd43af091e954cee9608160eea8599c95680
AndreiP <paduraru.andrei5@gmail.com>
2023-11-14 18:36:50 +0000

    temporary fix for the while(true) serial manager

--- ricardobackend/serialmanager/serialmanager.py
+++ ricardobackend/serialmanager/serialmanager.py
@@ -76,6 +76,7 @@ class SerialManager():
 					self.__checkSendQueue__()
 					self.__readPacket__()
 					self.__cleanupPacketRecord__()
+					# time.sleep(0.001)
 				except (OSError, serial.SerialException):
 					self.__auto_connect__()
 
